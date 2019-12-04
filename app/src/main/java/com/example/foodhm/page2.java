cc52619ecaa2aa82505826477f7d32c134cbbf55
kritsaran25 <38246323+kritsaran25@users.noreply.github.com>
2019-12-05 00:24:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/foodhm/page2.java
@@ -0,0 +1,14 @@
+package com.example.foodhm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class page2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+}
